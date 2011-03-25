83d3df0013aa991591e7e0cf123700908e2e78ab
harunzafer@gmail.com <harunzafer@gmail.com@fadc3922-203f-1dfc-9971-3e722d99108c>
2011-03-25 16:56:53 +0000

    - MAT.java sınıfı yazılmaya başlandı. - permutasyon ve combinasyon olayını halletmemiz gerekiyor.
    git-svn-id: http://javanta.googlecode.com/svn/trunk@21 fadc3922-203f-1dfc-9971-3e722d99108c

--- test/Main.java
+++ test/Main.java
@@ -1,13 +1,11 @@
 
-import com.hrzafer.javanta.DB;
 import com.hrzafer.javanta.GUI;
 import com.hrzafer.javanta.IO;
+import com.hrzafer.javanta.MAT;
 import com.hrzafer.javanta.STR;
 import java.io.File;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
  * To change this template, choose Tools | Templates
@@ -69,14 +67,59 @@ public class Main {
         //System.out.println(IO.readWords("test_UTF8.txt"));
         //System.out.println(IO.readWords("test_ISO.txt",IO.ISO_TR));
 
-        ResultSet rs = DB.executeQuery("SELECT * FROM `admin`");
-        try {
-            rs.next();
-            System.out.println(rs.getString("username"));
-            rs.next();
-            System.out.println(rs.getString("username"));
-        } catch (SQLException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        ResultSet rs = DB.executeQuery("SELECT * FROM `admin`");
+//        try {
+//            rs.next();
+//            System.out.println(rs.getString("username"));
+//            rs.next();
+//            System.out.println(rs.getString("username"));
+//        } catch (SQLException ex) {
+//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//
+
+        System.out.println(STR.toNonTurkish("Bu nedenlerle, geliştirilecek Beyan Akıllı Yazım Desteği yazılımının ülkemizde büyük bir ihtiyacı gidereceği düşünülmektedir. Yazılımın Türk kullanıcıların büyük çoğunluğu tarafından kullanılan MS Word metin editörüne entegre bir biçimde çalışması planlanmaktadır. Bu şekilde yazılımın çok daha fazla kullanıcıya hitap edeceği düşünülmektedir. Böyle bir yazılıma başta eğitim kurumları ve resmi kurumlar olmak üzere pek çok sektörden talep geleceği öngörülmektedir."));
+
+
+        //Set up some lists
+        List<String> l1 = new ArrayList<String>();
+        l1.add("A1");
+        l1.add("A2");
+
+        List<String> l2 = new ArrayList<String>();
+        l2.add("B1");
+        l2.add("B2");
+        l2.add("B3");
+
+        List<String> l3 = new ArrayList<String>();
+        l3.add("C1");
+
+        List<String> l4 = new ArrayList<String>();
+        l4.add("D1");
+        l4.add("D2");
+        
+        List<List<String>> ll = new ArrayList<List<String>>();
+
+        ll.add(l1);
+        ll.add(l2);
+        ll.add(l3);
+        ll.add(l4);
+
+        for (List<String> list : ll) {
+            for (String string : list) {
+                System.out.print(string + ",");
+            }
+            System.out.println("");
+        }
+
+
+        ll = MAT.combinate(ll);
+
+        for (List<String> list : ll) {
+            for (String string : list) {
+                System.out.print(string + ",");
+            }
+            System.out.println("");
         }
 
     }
